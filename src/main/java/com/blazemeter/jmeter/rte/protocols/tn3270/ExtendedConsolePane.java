6fcab8abec85f70dd4b5ef1208d14b2824772cdd
Luis Zambra <luis.zambra@abstracta.com.uy>
2018-04-16 12:26:26 -0300

    Entended TelnetState, added more logs and checked the exceptions from ExtendedTerminalServer

--- src/main/java/com/blazemeter/jmeter/rte/protocols/tn3270/ExtendedConsolePane.java
+++ src/main/java/com/blazemeter/jmeter/rte/protocols/tn3270/ExtendedConsolePane.java
@@ -1,41 +1,31 @@
 package com.blazemeter.jmeter.rte.protocols.tn3270;
 
+import com.blazemeter.jmeter.rte.core.RteIOException;
 import com.blazemeter.jmeter.rte.core.ssl.SSLType;
-import com.blazemeter.jmeter.rte.protocols.ReflectionUtils;
 import com.bytezone.dm3270.application.ConsolePane;
 import com.bytezone.dm3270.display.Screen;
 import com.bytezone.dm3270.plugins.PluginsStage;
 import com.bytezone.dm3270.streams.TelnetListener;
-import com.bytezone.dm3270.streams.TelnetState;
-import com.bytezone.dm3270.streams.TerminalServer;
 import com.bytezone.dm3270.utilities.Site;
-import java.lang.reflect.Method;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- * This is an extension of {@link ConsolePane} but making connect method public.
+ * This is an extension of {@link ConsolePane} that provides connect method public visibility and
+ * SSL / connection timeout support. Apart from that, instead of handling the exceptions on the
+ * class itself, they are thrown to tn3270class.
  */
-public class ConnectibleConsolePane extends ConsolePane {
+public class ExtendedConsolePane extends ConsolePane {
 
-  private static final Logger LOG = LoggerFactory.getLogger(ConnectibleConsolePane.class);
-  private TelnetListener telnetListener;
-  private final TelnetState telnetState;
   private final Site server;
-  private TerminalServer terminalServer;
+  private ExtendedTerminalServer terminalServer;
   private final Screen screen;
   private Thread terminalServerThread;
   private SSLType sslType;
   private int connectionTimeoutMillis;
 
-  private static final Method CONNECT_METHOD = ReflectionUtils
-      .getAccessibleMethod(ConsolePane.class, "connect");
-
-  public ConnectibleConsolePane(Screen screen, Site server, PluginsStage pluginsStage) {
+  public ExtendedConsolePane(Screen screen, Site server, PluginsStage pluginsStage) {
     super(screen, server, pluginsStage);
     this.server = server;
     this.screen = screen;
-    this.telnetState = screen.getTelnetState();
   }
 
   public void setSslType(SSLType sslType) {
@@ -52,10 +42,11 @@ public class ConnectibleConsolePane extends ConsolePane {
     }
 
     // set preferences for this session
+    ExtendedTelnetState telnetState = (ExtendedTelnetState) screen.getTelnetState();
     telnetState.setDo3270Extended(server.getExtended());
     telnetState.setDoTerminalType(true);
 
-    telnetListener = new TelnetListener(screen, telnetState);
+    TelnetListener telnetListener = new TelnetListener(screen, telnetState);
     terminalServer =
         new ExtendedTerminalServer(server.getURL(), server.getPort(), telnetListener, sslType,
             connectionTimeoutMillis);
@@ -65,23 +56,21 @@ public class ConnectibleConsolePane extends ConsolePane {
     terminalServerThread.start();
   }
 
-  @Override
-  public void disconnect() {
+  public void doDisconnect() throws InterruptedException {
     if (terminalServer != null) {
       terminalServer.close();
     }
 
-    telnetState.close();
+    screen.getTelnetState().close();
 
     if (terminalServerThread != null) {
       terminalServerThread.interrupt();
-      try {
-        terminalServerThread.join();
-      } catch (InterruptedException ex) {
-        terminalServerThread.interrupt();
-        LOG.info("Communication error with Rte server, the disconnection process was interrupted.");
-      }
+      terminalServerThread.join();
     }
+  }
+
+  public synchronized void throwAnyPendingError() throws RteIOException {
+    terminalServer.throwAnyPendingError();
   }
 
 }
--- src/main/java/com/blazemeter/jmeter/rte/protocols/tn3270/ExtendedTerminalServer.java
+++ src/main/java/com/blazemeter/jmeter/rte/protocols/tn3270/ExtendedTerminalServer.java
@@ -1,10 +1,11 @@
 package com.blazemeter.jmeter.rte.protocols.tn3270;
 
+import com.blazemeter.jmeter.rte.core.RteIOException;
+import com.blazemeter.jmeter.rte.core.ssl.SSLSocketFactory;
+import com.blazemeter.jmeter.rte.core.ssl.SSLType;
 import com.bytezone.dm3270.streams.BufferListener;
-import com.bytezone.dm3270.streams.TerminalServer;
 import com.bytezone.dm3270.streams.TelnetSocket.Source;
-import com.bytezone.dm3270.utilities.Dm3270Utility;
-
+import com.bytezone.dm3270.streams.TerminalServer;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -12,15 +13,18 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.security.GeneralSecurityException;
 import java.time.LocalDateTime;
-
-import com.blazemeter.jmeter.rte.core.ssl.SSLSocketFactory;
-import com.blazemeter.jmeter.rte.core.ssl.SSLType;
+import javax.net.ssl.SSLException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Performs the same as {@link TerminalServer}, but in this case uses a socket that supports SSL and
+ * connection timeout. Apart from that, instead of handling the exceptions on the class itself, they
+ * are thrown to tn3270class.
+ */
 public class ExtendedTerminalServer extends TerminalServer {
 
-  private final Logger LOG = LoggerFactory.getLogger(ExtendedTerminalServer.class);
+  private static final Logger LOG = LoggerFactory.getLogger(ExtendedTerminalServer.class);
   private int connectionTimeoutMillis;
   private SSLType sslType;
 
@@ -31,11 +35,11 @@ public class ExtendedTerminalServer extends TerminalServer {
   private OutputStream serverOut;
 
   private final byte[] buffer = new byte[4096];
-  private int bytesRead;
   private volatile boolean running;
 
-  private final boolean debug = false;
   private final BufferListener telnetListener;
+
+  private Throwable pendingError;
 
   public ExtendedTerminalServer(String serverURL, int serverPort, BufferListener listener,
       SSLType sslType, int connectionTimeoutMillis) {
@@ -45,6 +49,39 @@ public class ExtendedTerminalServer extends TerminalServer {
     this.serverURL = serverURL;
     this.connectionTimeoutMillis = connectionTimeoutMillis;
     this.telnetListener = listener;
+  }
+
+  @Override
+  public void run() {
+    try {
+      socket = createSocket();
+      serverIn = socket.getInputStream();
+      serverOut = socket.getOutputStream();
+      running = true;
+
+      while (running) {
+
+        int bytesRead = serverIn.read(buffer);
+        if (bytesRead < 0) {
+          close();
+          break;
+        }
+
+        byte[] message = new byte[bytesRead];
+        System.arraycopy(buffer, 0, message, 0, bytesRead);
+        telnetListener.listen(Source.SERVER, message, LocalDateTime.now(), true);
+      }
+    } catch (GeneralSecurityException ex) {
+      if (running) {
+        close();
+      }
+      setPendingError(new SSLException(ex));
+    } catch (IOException ex) {
+      if (running) {
+        close();
+        setPendingError(ex);
+      }
+    }
   }
 
   private Socket createSocket() throws IOException, GeneralSecurityException {
@@ -59,45 +96,26 @@ public class ExtendedTerminalServer extends TerminalServer {
     }
   }
 
-  @Override
-  public void run() {
+  public synchronized void write(byte[] buffer) {
+    if (serverOut == null) {
+      // the no-op may come here if the program is not closed after disconnection
+      LOG.debug("serverOut is null in TerminalServer");
+      return;
+    }
+
     try {
-      socket = createSocket();
-      serverIn = socket.getInputStream();
-      serverOut = socket.getOutputStream();
-      running = true;
+      serverOut.write(buffer);
+      serverOut.flush();
+    } catch (IOException e) {
+      setPendingError(e);
+    }
+  }
 
-      while (running) {
-        if (Thread.interrupted()) {
-          LOG.info("TerminalServer interrupted");
-          break;
-        }
-
-        bytesRead = serverIn.read(buffer);
-        if (bytesRead < 0) {
-          close();
-          break;
-        }
-
-        if (Thread.currentThread().isInterrupted()) {
-          LOG.info("TerminalServer was interrupted");
-        }
-
-        if (debug) {
-          LOG.info(toString());
-          LOG.info("reading:");
-          LOG.info(Dm3270Utility.toHex(buffer, 0, bytesRead));
-        }
-
-        byte[] message = new byte[bytesRead];
-        System.arraycopy(buffer, 0, message, 0, bytesRead);
-        telnetListener.listen(Source.SERVER, message, LocalDateTime.now(), true);
-      }
-    } catch (GeneralSecurityException | IOException ex) {
-      if (running) {
-        LOG.info("Communication with Rte server failed.");
-        close();
-      }
+  private synchronized void setPendingError(Throwable ex) {
+    if (pendingError == null) {
+      pendingError = ex;
+    } else {
+      LOG.error("Exception ignored in step result due to previously thrown exception", ex);
     }
   }
 
@@ -105,6 +123,7 @@ public class ExtendedTerminalServer extends TerminalServer {
   public void close() {
     try {
       running = false;
+
       serverIn = null;
       serverOut = null;
 
@@ -116,7 +135,16 @@ public class ExtendedTerminalServer extends TerminalServer {
         telnetListener.close();
       }
     } catch (IOException ex) {
-      LOG.info("Communication with Rte server failed.");
+      setPendingError(ex);
+      LOG.error("Communication with rte server failed.");
+    }
+  }
+
+  public synchronized void throwAnyPendingError() throws RteIOException {
+    if (pendingError != null) {
+      Throwable ret = pendingError;
+      pendingError = null;
+      throw new RteIOException(ret);
     }
   }
 
