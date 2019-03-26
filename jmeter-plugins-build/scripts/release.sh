a28ee36dcca2deb19ab965209a4a079fc7984f64
Roger Abelenda <roger.abelenda@abstracta.com.uy>
2019-03-26 11:43:07 -0300

    Remove unnecessary command in release script due to maven bug fixing
    Additionally, update opened since 9 is no longer available in Debian repository.

--- jmeter-plugins-build/scripts/release.sh
+++ jmeter-plugins-build/scripts/release.sh
@@ -20,7 +20,5 @@ mvn --batch-mode scm:check-local-modification -DpushChanges=false
 mvn --batch-mode scm:tag -Dtag="\${project.version}"
 
 git checkout master && git merge production
-# we need this due to https://github.com/mojohaus/versions-maven-plugin/issues/207
-mvn --batch-mode versions:set -DnextSnapshot=true -DnewVersion=$ORIGINAL_VERSION
 mvn --batch-mode versions:set -DnextSnapshot=true
 mvn --batch-mode scm:checkin -Dmessage="[RELEASE][skip ci] Increase version to next development version \${project.version}" -Dincludes=pom.xml
