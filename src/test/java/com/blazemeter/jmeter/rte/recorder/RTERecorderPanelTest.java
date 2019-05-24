ae8ac2492c2e86b8d8d540a0a3b4dcf4b2031df9
rpoleo <ricardo.poleo@cl.abstracta.us>
2019-05-24 15:09:50 -0400

    Added basic Test structure

---
+++ src/test/java/com/blazemeter/jmeter/rte/recorder/RTERecorderPanelTest.java
@@ -0,0 +1,58 @@
+package com.blazemeter.jmeter.rte.recorder;
+
+import org.junit.Test;
+
+public class RTERecorderPanelTest {
+
+  @Test
+  public void shouldNotifyStartRecordingListenerWhenStartRecording(){
+
+  }
+
+  @Test
+  public void shouldNotifyStopRecordingListenerWhenStopRecording(){}
+
+  @Test
+  public void shouldNotifyStopAndStartRecordingListenerWhenRestartRecording(){}
+
+  @Test
+  public void shouldDisableStartButtonWhenStartRecording(){}
+
+  @Test
+  public void shouldEnableStopButtonWhenStartRecording(){}
+
+  @Test
+  public void shouldEnableRestartButtonWhenStartRecording(){}
+
+  @Test
+  public void shouldHaveEnabledStartButtonWhenInitPanel(){}
+
+  @Test
+  public void shouldHaveDisabledStopButtonWhenInitPanel(){}
+
+  @Test
+  public void shouldHaveDisabledRestartButtonWhenInitPanel(){}
+
+  @Test
+  public void shouldEnableStartButtonWhenStopRecording(){}
+
+  @Test
+  public void shouldDisableStopButtonWhenStopRecording(){}
+
+  @Test
+  public void shouldDisableRestartButtonWhenStopRecording(){}
+
+  /**
+   * Roger: los ultimos 2 van a quedar como varios asserts,
+   * si queres podes usar soft asserts ahi. Este tiene que
+   * ser un test IT con interfaz grafica (assertjswing)
+   * */
+  @Test
+  public void shouldGetConfiguredProperiesWhenFieldsAreSet(){}
+
+  @Test
+  public void shouldGetConfiguredFieldsWhenPropertiesAreSet(){}
+
+
+
+}
