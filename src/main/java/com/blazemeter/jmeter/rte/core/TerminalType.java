7fe648187210a94765da731467ef25ecf0c78379
Joaquin Araujo <joaquin.araujo@abstracta.com.uy>
2019-08-22 14:56:57 -0300

    RTE-60 Modifications in terminal type in order to cast from string

--- src/main/java/com/blazemeter/jmeter/rte/core/TerminalType.java
+++ src/main/java/com/blazemeter/jmeter/rte/core/TerminalType.java
@@ -2,10 +2,13 @@ package com.blazemeter.jmeter.rte.core;
 
 import java.awt.Dimension;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TerminalType {
 
   public static final String SCREEN_SIZE_SEPARATOR = "x";
+  private static final Pattern DIMENSION_SCREEN_PATTERN = Pattern.compile("^(\\d+)x(\\d+)$");
   private String id;
   private Dimension screenSize;
 
@@ -16,6 +19,15 @@ public class TerminalType {
   public TerminalType(String id, Dimension screenSize) {
     this.id = id;
     this.screenSize = screenSize;
+  }
+
+  public static Dimension fromString(String text) {
+    Matcher m = DIMENSION_SCREEN_PATTERN.matcher(text);
+    if (m.matches()) {
+      return new Dimension(Integer.parseInt(m.group(1)), Integer.parseInt(m.group(2)));
+    } else {
+      throw new IllegalArgumentException("The text '" + text + "' does not match dimension format");
+    }
   }
 
   public String getId() {
