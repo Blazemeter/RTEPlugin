1e841227be3bfde373885e4e27d8fce3b063a5af
Joaquin Araujo <joaquin.araujo@abstracta.com.uy>
2019-08-26 10:52:16 -0300

    RTE-60 adding logic to search for a close field
    current logic is looking for closer field on the right when the offset value is positive (meaning that we are going to skip forward a field). In the other hand when offset value is negative the logic will search for the closer field on the left. In remote case, offset has value of zero, then it will search for closer field on the right.

--- src/main/java/com/blazemeter/jmeter/rte/extractor/RTEExtractor.java
+++ src/main/java/com/blazemeter/jmeter/rte/extractor/RTEExtractor.java
@@ -3,18 +3,23 @@ package com.blazemeter.jmeter.rte.extractor;
 import com.blazemeter.jmeter.rte.core.Position;
 import com.blazemeter.jmeter.rte.core.RteSampleResultBuilder;
 import com.blazemeter.jmeter.rte.core.TerminalType;
-import com.blazemeter.jmeter.rte.core.exceptions.InvalidFieldPositionException;
 import com.helger.commons.annotation.VisibleForTesting;
 import java.awt.Dimension;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.apache.jmeter.processor.PostProcessor;
 import org.apache.jmeter.testelement.AbstractScopedTestElement;
 import org.apache.jmeter.threads.JMeterContext;
 import org.apache.jmeter.threads.JMeterVariables;
 import org.apache.jmeter.util.JMeterUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RTEExtractor extends AbstractScopedTestElement implements PostProcessor {
+
+  private static final Logger LOG = LoggerFactory.getLogger(RTEExtractor.class);
 
   private static final String ROW_PROPERTY = "RTEExtractor.row";
   private static final String COLUMN_PROPERTY = "RTEExtractor.column";
@@ -22,8 +27,6 @@ public class RTEExtractor extends AbstractScopedTestElement implements PostProce
   private static final String VARIABLE_PREFIX_PROPERTY = "RTEExtractor.variablePrefix";
   private static final String POSITION_TYPE_PROPERTY = "RTEExtractor.positionType";
   private static final PositionType DEFAULT_POSITION_TYPE = PositionType.CURSOR_POSITION;
-
-  private boolean testing;
   private JMeterContext context;
 
   public RTEExtractor() {
@@ -32,7 +35,7 @@ public class RTEExtractor extends AbstractScopedTestElement implements PostProce
 
   @Override
   public void process() {
-    context = testing ? context : getThreadContext();
+    context = context != null ? context : getThreadContext();
     JMeterVariables vars = context.getVariables();
     Position position = extractPosition(context.getPreviousResult().getResponseHeaders());
     String variablePrefix = validateVariablePrefix(getVariablePrefix());
@@ -42,7 +45,7 @@ public class RTEExtractor extends AbstractScopedTestElement implements PostProce
 
   private String validateVariablePrefix(String variablePrefix) {
     if (variablePrefix.isEmpty()) {
-      JMeterUtils.reportErrorToUser("The variable name in extractor is essential for later usage");
+      LOG.warn("The variable name in extractor is essential for later usage");
     }
     return variablePrefix;
   }
@@ -51,11 +54,11 @@ public class RTEExtractor extends AbstractScopedTestElement implements PostProce
     if (getPositionType() == PositionType.CURSOR_POSITION) {
       return extractCursorPosition(responseHeaders);
     } else {
-      return getFieldPosition(responseHeaders);
+      return extractFieldPosition(responseHeaders);
     }
   }
 
-  private Position getFieldPosition(String responseHeaders) {
+  private Position extractFieldPosition(String responseHeaders) {
     Position effectivePosition = null;
     int startFieldsPosition =
         responseHeaders.indexOf(RteSampleResultBuilder.FIELDS_POSITION_HEADER)
@@ -66,50 +69,101 @@ public class RTEExtractor extends AbstractScopedTestElement implements PostProce
     String fieldsPositionAsText = responseHeaders
         .substring(startFieldsPosition, lastFieldsPosition);
 
-    List<Position> fieldPositions = new ArrayList<>();
-    for (String field : fieldsPositionAsText
-        .split(RteSampleResultBuilder.FIELD_POSITION_SEPARATOR)) {
-      fieldPositions.add(Position.fromString(field));
-    }
+    List<Position> fieldPositions = Arrays
+        .stream(fieldsPositionAsText.split(RteSampleResultBuilder.FIELD_POSITION_SEPARATOR))
+        .map(Position::fromString)
+        .collect(Collectors.toList());
 
-    try {
-      if (isGivenFieldPositionValid(context.getPreviousResult().getRequestHeaders())) {
+    String requestHeaders = context.getPreviousResult().getRequestHeaders();
+    if (isGivenFieldPositionValid(requestHeaders)) {
+      Position givenPosition = new Position(getRowAsInt(), getColumnAsInt());
 
-        int positionIndex = fieldPositions
-            .indexOf(new Position(getRowAsInt(), getColumnAsInt()));
-        if (positionIndex != -1) {
-          effectivePosition = fieldPositions
-              .get(positionIndex + Integer.parseInt(getOffset()));
-        } else {
-          String cause = "Inserted values for row/column in extractor\n"
-              + "do not match with any field in current screen";
-          JMeterUtils.reportErrorToUser(cause);
+      effectivePosition = getNewPosition(givenPosition, fieldPositions);
 
-          throw new InvalidFieldPositionException(new Position(getRowAsInt(), getColumnAsInt()),
-              new Throwable(cause));
-        }
-      } else {
-        JMeterUtils.reportErrorToUser(
-            "Inserted values for row and column in extractor\n"
-                + "do not match with the screen size.");
-      }
-
-    } catch (IndexOutOfBoundsException e) {
+    } else {
       JMeterUtils.reportErrorToUser(
-          "Number of fields in the screen was " + fieldPositions.size()
-              + "\nTherefore is not possible to skip "
-              + getOffset() + " fields");
+          "Inserted values for row and column in extractor\n"
+              + "do not match with the screen size.");
+
     }
+
     return effectivePosition;
+  }
+
+  private Position getNewPosition(Position givenPosition,
+      List<Position> fieldPositions) {
+    int givenLinearPosition = buildLinealPosition(givenPosition);
+    List<Integer> fieldsLinearPosition = fieldPositions.stream()
+        .map(this::buildLinealPosition)
+        .collect(Collectors.toList());
+    int offset = Integer.parseInt(getOffset());
+
+    if (offset > 0) {
+      int nextField = getForwardLinearPositionField(fieldsLinearPosition, givenLinearPosition);
+      try {
+        return fieldPositions.get(fieldsLinearPosition.indexOf(nextField) + offset);
+      } catch (ArrayIndexOutOfBoundsException e) {
+        e.printStackTrace();
+        LOG.warn("Number of fields in the screen was " + fieldPositions.size()
+            + "\nTherefore is not possible to skip "
+            + getOffset() + " fields");
+      }
+    } else if (offset < 0) {
+
+      int nextField = getBackwardLinearPositionField(fieldsLinearPosition, givenLinearPosition);
+
+      try {
+        return fieldPositions.get(fieldsLinearPosition.indexOf(nextField) + offset);
+      } catch (ArrayIndexOutOfBoundsException e) {
+        e.printStackTrace();
+        LOG.warn("Number of fields in the screen was " + fieldPositions.size()
+            + "\nTherefore is not possible to go backwards "
+            + Math.abs(Integer.parseInt(getOffset())) + " fields");
+      }
+    } else {
+      int nextField = getForwardLinearPositionField(fieldsLinearPosition, givenLinearPosition);
+      return fieldPositions.get(fieldsLinearPosition.indexOf(nextField) + offset);
+    }
+    return givenPosition;
+
+  }
+
+  private int getBackwardLinearPositionField(List<Integer> fieldsLinearPosition,
+      int givenLinearPosition) {
+    fieldsLinearPosition.sort(Collections.reverseOrder());
+    for (Integer field : fieldsLinearPosition) {
+      if (field == givenLinearPosition) {
+        return field;
+      } else if (field < givenLinearPosition) {
+        return field;
+      }
+    }
+    LOG.warn("There is not fields position in the right close of the given position");
+    return -1;
+  }
+
+  private int getForwardLinearPositionField(List<Integer> fieldsLinearPosition,
+      int givenLinearPosition) {
+    for (Integer field : fieldsLinearPosition) {
+      if (field == givenLinearPosition) {
+        return field;
+      } else if (field > givenLinearPosition) {
+        return field;
+      }
+    }
+    LOG.warn("There is not fields position in the right close of the given position");
+    return -1;
   }
 
   private Position extractCursorPosition(String responseHeaders) {
     Position effectivePosition;
-    int cursorPositionStart = responseHeaders.indexOf(RteSampleResultBuilder.CURSOR_POSITION_HEADER)
-        + RteSampleResultBuilder.CURSOR_POSITION_HEADER.length();
+    int cursorPositionStart =
+        responseHeaders.indexOf(RteSampleResultBuilder.CURSOR_POSITION_HEADER)
+            + RteSampleResultBuilder.CURSOR_POSITION_HEADER.length();
     String cursorPositionAsText = responseHeaders
         .substring(cursorPositionStart,
-            responseHeaders.indexOf(RteSampleResultBuilder.HEADERS_SEPARATOR, cursorPositionStart));
+            responseHeaders
+                .indexOf(RteSampleResultBuilder.HEADERS_SEPARATOR, cursorPositionStart));
     effectivePosition = Position.fromString(cursorPositionAsText);
     return effectivePosition;
   }
@@ -122,21 +176,16 @@ public class RTEExtractor extends AbstractScopedTestElement implements PostProce
   }
 
   private Dimension getScreenDimensions(String requestHeaders) {
-    return TerminalType.fromString(extractScreenSize(requestHeaders));
+    return TerminalType.fromString(extractTerminalType(requestHeaders)).getScreenSize();
   }
 
-  private String extractScreenSize(String requestHeaders) {
+  private String extractTerminalType(String requestHeaders) {
     int terminalTypeIndex =
         requestHeaders.indexOf(RteSampleResultBuilder.HEADERS_TERMINAL_TYPE)
             + RteSampleResultBuilder.HEADERS_TERMINAL_TYPE.length();
     int endOfScreenDimension = requestHeaders
         .indexOf(RteSampleResultBuilder.HEADERS_SEPARATOR, terminalTypeIndex);
-    String terminalTypeHeader = requestHeaders.substring(terminalTypeIndex, endOfScreenDimension);
-    int lastNonBlankPosition = terminalTypeHeader.length() - 1;
-    while (lastNonBlankPosition >= 0 && (terminalTypeHeader.charAt(lastNonBlankPosition) != ' ')) {
-      lastNonBlankPosition--;
-    }
-    return terminalTypeHeader.substring(lastNonBlankPosition + 1);
+    return requestHeaders.substring(terminalTypeIndex, endOfScreenDimension);
   }
 
   private int getRowAsInt() {
@@ -188,9 +237,13 @@ public class RTEExtractor extends AbstractScopedTestElement implements PostProce
     setProperty(POSITION_TYPE_PROPERTY, positionType.name());
   }
 
+  private int buildLinealPosition(Position position) {
+    Dimension size = getScreenDimensions(context.getPreviousResult().getRequestHeaders());
+    return size.width * (position.getRow() - 1) + position.getColumn() - 1;
+  }
+
   @VisibleForTesting
   public void setContext(JMeterContext context) {
-    testing = true;
     this.context = context;
   }
 }
